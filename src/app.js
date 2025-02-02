6cce5fccdf7ca5790eccfdd5f686e093ff97e10f
deekshasingh1309 <deekshasingh1309@gmail.com>
2025-02-02 22:01:06 +0530

    order changed

--- src/app.js
+++ src/app.js
@@ -5,25 +5,34 @@ const app=express();
 
 //to handle to code 
 //request handler function
-// app.use("/",(req,res)=>{
-//     //respond to the server
-//     res.send("In the main page"); //whatever request is coming we are sending the same response if route is not passed  
-// })
 
 app.use("/text",(req,res)=>{
     //respond to the server
     res.send("In the text server "); //based on route  
 });
+
 //handling based on routes
+/* Order of routes matters a lot --- otherwise routes won't work properly gives the first match reponse- It checks from top*/
+app.use("/hello/1",(req,res)=>{
+    //respond to the server
+    res.send("Hello 1 1 1 ..");
+});
+
 app.use("/hello",(req,res)=>{
     //respond to the server
     res.send("Hello hello ..");
 });
 
+
 app.use("/world",(req,res)=>{
-    //respond to the server
     res.send("World server");
 });
+
+app.use("/",(req,res)=>{
+    //respond to the server
+    res.send("In the main page"); //whatever request is coming we are sending the same response if route is not passed  
+});
+
 //server is listening on port no 3000
 app.listen(7777,()=>{
     console.log("Server is running on port 7777");
